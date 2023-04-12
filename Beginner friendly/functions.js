f58a0e1b88f9d4495a3bb01af8f3b0dd56b3492f
D3vilOPエース <97296994+DevilexeOP@users.noreply.github.com>
2023-04-12 22:21:07 +0530

    updated function.js

--- Beginner friendly/functions.js
+++ Beginner friendly/functions.js
@@ -1,1 +1,13 @@
-function add
+function addNums(nums1 = 3, nums2 = 2) {
+  return nums1 + nums2;
+}
+
+console.log(addNums(6, 5));
+
+// es6
+
+const subtractNums = (nums1 = 3, nums2 = 2) => {
+  return nums1 - nums2;
+};
+
+console.log(subtractNums(6, 5));
