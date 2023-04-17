3090fe62c78e0287838612a6fad356c9297043c1
D3vilOPエース <97296994+DevilexeOP@users.noreply.github.com>
2023-04-17 17:02:22 +0530

    added DOM and OOPs

--- Beginner friendly/oops.js
+++ Beginner friendly/oops.js
@@ -1,22 +1,36 @@
-function Person(firstName, lastName, dob) {
-  this.firstName = firstName;
-  this.lastName = lastName;
-  this.dob = new Date(dob);
-  //   this.getBirthDate = function () {
-  //     return this.dob.getDate();
-  //   };
-  //   this.fullName = function () {
-  //     return `${this.firstName} ${this.lastName}`;
-  //   };
+// function Person(firstName, lastName, dob) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.dob = new Date(dob);
+//     this.getBirthDate = function () {
+//       return this.dob.getDate();
+//     };
+//     this.fullName = function () {
+//       return `${this.firstName} ${this.lastName}`;
+//     };
+// }
+
+// Person.prototype.getBirthDate = function () {
+//   return this.dob.getDate();
+// };
+
+// Person.prototype.fullName = function () {
+//   return `${this.firstName} ${this.lastName}`;
+// };
+
+class Person {
+  constructor(firstName, lastName, dob) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.dob = new Date(dob);
+  }
+  getBirthDate() {
+    return this.dob.getDate();
+  }
+  fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
 }
-
-Person.prototype.getBirthDate = function () {
-  return this.dob.getDate();
-};
-
-Person.prototype.fullName = function () {
-  return `${this.firstName} ${this.lastName}`;
-};
 
 const person1 = new Person("Devil", "op", "09-10-2005");
 const person2 = new Person("Jod", "bbg", "5-2-2007");
